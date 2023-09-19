e1e4bbf54ccc6dbc199564a4153b741d57413b5a
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-19 19:20:09 +0800

    chore: remove redundant dep

--- packages/eslint-plugin-ts/util/getESLintCoreRule.ts
+++ packages/eslint-plugin-ts/util/getESLintCoreRule.ts
@@ -1,19 +1,8 @@
-import { ESLintUtils } from '@typescript-eslint/utils'
-import { version } from 'eslint/package.json'
-import * as semver from 'semver'
-
-// @ts-expect-error no types
 import jsRules from '@stylistic/eslint-plugin-js'
 import type { Rule } from 'eslint'
-
-const isESLintV8 = semver.major(version) >= 8
 
 export const getESLintCoreRule: (ruleId: string) => Rule.RuleModule = (ruleId) => {
   if (ruleId in jsRules.rules)
     return jsRules.rules[ruleId]()
-
-  // TODO: remove this as we may not need to depens on any rules from eslint
-  return isESLintV8
-    ? ESLintUtils.nullThrows(require('eslint/use-at-your-own-risk').builtinRules.get(ruleId), `ESLint's core rule '${ruleId}' not found.`)
-    : require(`eslint/lib/rules/${ruleId}`)
+  throw new Error(`Failed to find core rule ${ruleId}, this is an internal bug of @stylistic/eslint-plugin-ts`)
 }
