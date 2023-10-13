f9d08299f28d2e4861dd15e9dcf1d5485118c522
Shinigami <chrissi92@hotmail.de>
2023-10-13 02:19:00 +0200

    feat: define-config-support files (#20)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@stylistic/eslint-plugin-js': fileURLToPath(new URL('./packages/eslint-plugin-js/src/index.js', import.meta.url)),
+      '@stylistic/eslint-plugin-ts': fileURLToPath(new URL('./packages/eslint-plugin-ts/src/index.ts', import.meta.url)),
+    },
+  },
   test: {
     globals: true,
     reporters: 'dot',
