6e2ee810fc4baa05a372cfdad7eec786d906bb5d
Plumbiu <3434909403@qq.com>
2023-11-05 20:16:34 +0800

    feat: add doc comment for rule options

--- packages/eslint-plugin-jsx/dts/rule-options.d.ts
+++ packages/eslint-plugin-jsx/dts/rule-options.d.ts
@@ -18,24 +18,100 @@ import type { RuleOptions as JsxWrapMultilinesRuleOptions } from '../rules/jsx-w
 import type { RuleOptions as SelfClosingCompRuleOptions } from '../rules/self-closing-comp/types'
 
 export interface RuleOptions {
+  /**
+   * Enforce or disallow spaces inside of curly braces in JSX attributes and expressions.
+   * @see {@link https://eslint.style/rules/jsx/jsx-child-element-spacing}
+   */
   '@stylistic/jsx/jsx-child-element-spacing': JsxChildElementSpacingRuleOptions
+  /**
+   * Enforce the closing bracket location for JSX multiline elements.
+   * @see {@link https://eslint.style/rules/jsx/jsx-closing-bracket-location}
+   */
   '@stylistic/jsx/jsx-closing-bracket-location': JsxClosingBracketLocationRuleOptions
+  /**
+   * Enforce the closing tag location for multiline JSX elements.
+   * For situations where JSX expressions are unnecessary, please refer to the {@link https://legacy.reactjs.org/docs/jsx-in-depth.html React doc} and {@link https://github.com/facebook/react/blob/v15.4.0-rc.3/docs/docs/02.3-jsx-gotchas.md#html-entities this page about JSX gotchas}.
+   * @see {@link https://eslint.style/rules/jsx/jsx-closing-tag-location}
+   */
   '@stylistic/jsx/jsx-closing-tag-location': JsxClosingTagLocationRuleOptions
+  /**
+   * Disallow unnecessary JSX expressions when literals alone are sufficient or enforce JSX expressions on literals in JSX children or attributes.
+   * For situations where JSX expressions are unnecessary, please refer to the {@link https://legacy.reactjs.org/docs/jsx-in-depth.html React doc} and {@link https://github.com/facebook/react/blob/v15.4.0-rc.3/docs/docs/02.3-jsx-gotchas.md#html-entities this page about JSX gotchas}.
+   * @see {@link https://eslint.style/rules/jsx/jsx-curly-brace-presence}
+   */
   '@stylistic/jsx/jsx-curly-brace-presence': JsxCurlyBracePresenceRuleOptions
+  /**
+   * Enforce consistent linebreaks in curly braces in JSX attributes and expressions.
+   * @see {@link https://eslint.style/rules/jsx/jsx-curly-newline}
+   */
   '@stylistic/jsx/jsx-curly-newline': JsxCurlyNewlineRuleOptions
+  /**
+   * Enforce or disallow spaces inside of curly braces in JSX attributes and expressions.
+   * @see {@link https://eslint.style/rules/jsx/jsx-curly-spacing}
+   */
   '@stylistic/jsx/jsx-curly-spacing': JsxCurlySpacingRuleOptions
+  /**
+   * Enforce or disallow spaces around equal signs in JSX attributes.
+   * @see {@link https://eslint.style/rules/jsx/jsx-equals-spacing}
+   */
   '@stylistic/jsx/jsx-equals-spacing': JsxEqualsSpacingRuleOptions
+  /**
+   * Enforce proper position of the first property in JSX.
+   * @see {@link https://eslint.style/rules/jsx/jsx-first-prop-new-line}
+   */
   '@stylistic/jsx/jsx-first-prop-new-line': JsxFirstPropNewLineRuleOptions
+  /**
+   * Enforce props indentation in JSX.
+   * @see {@link https://eslint.style/rules/jsx/jsx-indent-props}
+   */
   '@stylistic/jsx/jsx-indent-props': JsxIndentPropsRuleOptions
+  /**
+   * Enforce JSX indentation.
+   * Note: The fixer will fix whitespace and tabs indentation.
+   * @see {@link https://eslint.style/rules/jsx/jsx-indent}
+   */
   '@stylistic/jsx/jsx-indent': JsxIndentRuleOptions
+  /**
+   * Enforce maximum of props on a single line in JSX.
+   * @see {@link https://eslint.style/rules/jsx/jsx-max-props-per-line}
+   */
   '@stylistic/jsx/jsx-max-props-per-line': JsxMaxPropsPerLineRuleOptions
+  /**
+   * Require or prevent a new line after jsx elements and expressions
+   * @see {@link https://eslint.style/rules/jsx/jsx-newline}
+   */
   '@stylistic/jsx/jsx-newline': JsxNewlineRuleOptions
+  /**
+   * Require one JSX element per line.
+   * Note: The fixer will insert line breaks between any expression that are on the same line.
+   * @see {@link https://eslint.style/rules/jsx/jsx-one-expression-per-line}
+   */
   '@stylistic/jsx/jsx-one-expression-per-line': JsxOneExpressionPerLineRuleOptions
+  /**
+   * Disallow multiple spaces between inline JSX props.
+   * Enforces that there is exactly one space between all attributes and after tag name and the first attribute in the same line.
+   * @see {@link https://eslint.style/rules/jsx/jsx-props-no-multi-spaces}
+   */
   '@stylistic/jsx/jsx-props-no-multi-spaces': JsxPropsNoMultiSpacesRuleOptions
+  /**
+   * Enforce props alphabetical sorting.
+   * @see {@link https://eslint.style/rules/jsx/jsx-sort-props}
+   */
   '@stylistic/jsx/jsx-sort-props': JsxSortPropsRuleOptions
+  /**
+   * Enforce whitespace in and around the JSX opening and closing brackets.
+   * Enforce or forbid spaces after the opening bracket, before the closing bracket, before the closing bracket of self-closing elements, and between the angle bracket and slash of JSX closing or self-closing elements.
+   * @see {@link https://eslint.style/rules/jsx/jsx-tag-spacing}
+   */
   '@stylistic/jsx/jsx-tag-spacing': JsxTagSpacingRuleOptions
+  /**
+   * Disallow missing parentheses around multiline JSX.
+   * Wrapping multiline JSX in parentheses can improve readability and/or convenience.
+   * @see {@link https://eslint.style/rules/jsx/jsx-wrap-multilines}
+   */
   '@stylistic/jsx/jsx-wrap-multilines': JsxWrapMultilinesRuleOptions
   '@stylistic/jsx/self-closing-comp': SelfClosingCompRuleOptions
+
 }
 
 export interface UnprefixedRuleOptions {
--- packages/eslint-plugin-ts/dts/rule-options.d.ts
+++ packages/eslint-plugin-ts/dts/rule-options.d.ts
@@ -21,27 +21,131 @@ import type { RuleOptions as SpaceInfixOpsRuleOptions } from '../rules/space-inf
 import type { RuleOptions as TypeAnnotationSpacingRuleOptions } from '../rules/type-annotation-spacing/types'
 
 export interface RuleOptions {
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/block-spacing block-spacing} rule.
+   * This version adds support for TypeScript related blocks (interfaces, object type literals and enums).
+   * @see {@link https://eslint.style/rules/ts/block-spacing}
+   */
   '@stylistic/ts/block-spacing': BlockSpacingRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/brace-style brace-style} rule.
+   * It adds support for enum, interface, namespace and module declarations.
+   * @see {@link https://eslint.style/rules/ts/brace-style}
+   */
   '@stylistic/ts/brace-style': BraceStyleRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/comma-dangle comma-dangle} rule.
+   * It adds support for TypeScript syntax.
+   * See the {@link https://eslint.org/docs/latest/rules/comma-dangle ESLint documentation} for more details on the comma-dangle rule.
+   * @see {@link https://eslint.style/rules/ts/comma-dangle}
+   */
   '@stylistic/ts/comma-dangle': CommaDangleRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/comma-spacing comma-spacing} rule.
+   * It adds support for trailing comma in a types parameters list.
+   * @see {@link https://eslint.style/rules/ts/comma-spacing}
+   */
   '@stylistic/ts/comma-spacing': CommaSpacingRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/func-call-spacing func-call-spacing} rule.
+   * It adds support for generic type parameters on function calls.
+   * @see {@link https://eslint.style/rules/ts/func-call-spacing}
+   */
   '@stylistic/ts/func-call-spacing': FuncCallSpacingRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/indent indent} rule. It adds support for TypeScript nodes.
+   * Please read Issue {@link https://github.com/typescript-eslint/typescript-eslint/issues/1824 #1824: Problems with the indent rule} before using this rule!
+   * @see {@link https://eslint.style/rules/ts/indent}
+   */
   '@stylistic/ts/indent': IndentRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/key-spacing key-spacing} rule.
+   * It adds support for type annotations on interfaces, classes and type literals properties.
+   * @see {@link https://eslint.style/rules/ts/key-spacing}
+   */
   '@stylistic/ts/key-spacing': KeySpacingRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/keyword-spacing keyword-spacing} rule.
+   * It adds support for generic type parameters on function calls.
+   * @see {@link https://eslint.style/rules/ts/keyword-spacing}
+   */
   '@stylistic/ts/keyword-spacing': KeywordSpacingRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/lines-around-comment lines-around-comment} rule.
+   * It adds support for TypeScript syntax.
+   * See the {@link https://eslint.org/docs/latest/rules/lines-around-comment ESLint documentation} for more details on the lines-around-comment rule.
+   * @see {@link https://eslint.style/rules/ts/lines-around-comment}
+   */
   '@stylistic/ts/lines-around-comment': LinesAroundCommentRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/lines-between-class-members lines-between-class-members} rule.
+   * It adds support for ignoring overload methods in a class.
+   * @see {@link https://eslint.style/rules/ts/lines-between-class-members}
+   */
   '@stylistic/ts/lines-between-class-members': LinesBetweenClassMembersRuleOptions
+  /**
+   * This rule enforces keeping to one configurable code style. It can also standardize the presence (or absence) of a delimiter in the last member of a construct, as well as a separate delimiter syntax for single line declarations.
+   * @see {@link https://eslint.style/rules/ts/member-delimiter-style}
+   */
   '@stylistic/ts/member-delimiter-style': MemberDelimiterStyleRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/no-extra-parens no-extra-parens} rule.
+   * It adds support for TypeScript type assertions.
+   * @see {@link https://eslint.style/rules/ts/no-extra-parens}
+   */
   '@stylistic/ts/no-extra-parens': NoExtraParensRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/no-extra-semi no-extra-semi }rule. It adds support for class properties.
+   * @see {@link https://eslint.style/rules/ts/no-extra-semi}
+   */
   '@stylistic/ts/no-extra-semi': NoExtraSemiRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/object-curly-spacing object-curly-spacing} rule.
+   * It adds support for TypeScript's object types.
+   * @see {@link https://eslint.style/rules/ts/object-curly-spacing}
+   */
   '@stylistic/ts/object-curly-spacing': ObjectCurlySpacingRuleOptions
+  /**
+   * This rule extends the base
+   * @see {@link https://eslint.style/rules/ts/padding-line-between-statements}
+   */
   '@stylistic/ts/padding-line-between-statements': PaddingLineBetweenStatementsRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/padding-line-between-statements padding-line-between-statements} rule.
+   * It adds support for TypeScript constructs such as interface and type.
+   * @see {@link https://eslint.style/rules/ts/quotes}
+   */
   '@stylistic/ts/quotes': QuotesRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/semi semi} rule.
+   * It adds support for TypeScript features that require semicolons.
+   * @see {@link https://eslint.style/rules/ts/semi}
+   */
   '@stylistic/ts/semi': SemiRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/space-before-blocks space-before-blocks} rule.
+   * It adds support for interfaces and enums.
+   * @see {@link https://eslint.style/rules/ts/space-before-blocks}
+   */
   '@stylistic/ts/space-before-blocks': SpaceBeforeBlocksRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/space-before-function-paren space-before-function-paren }rule.
+   * It adds support for generic type parameters on function calls.
+   * @see {@link https://eslint.style/rules/ts/space-before-function-paren}
+   */
   '@stylistic/ts/space-before-function-paren': SpaceBeforeFunctionParenRuleOptions
+  /**
+   * This rule extends the base {@link https://eslint.style/rules/js/space-infix-ops space-infix-ops} rule.
+   * It adds support for enum members.
+   * @see {@link https://eslint.style/rules/ts/space-infix-ops}
+   */
   '@stylistic/ts/space-infix-ops': SpaceInfixOpsRuleOptions
+  /**
+   * This rule aims to enforce specific spacing patterns around type annotations and function types in type literals.
+   * @see {@link https://eslint.style/rules/ts/type-annotation-spacing}
+   */
   '@stylistic/ts/type-annotation-spacing': TypeAnnotationSpacingRuleOptions
+
 }
 
 export interface UnprefixedRuleOptions {
