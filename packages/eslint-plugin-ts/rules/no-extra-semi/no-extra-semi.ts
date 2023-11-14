bbb24c9c6976aceee76a8658a645ff1e142caca5
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-14 13:04:25 +0100

    refactor: improve code coverage

--- packages/eslint-plugin-ts/rules/no-extra-semi/no-extra-semi.ts
+++ packages/eslint-plugin-ts/rules/no-extra-semi/no-extra-semi.ts
@@ -23,21 +23,8 @@ export default createRule<RuleOptions, MessageIds>({
 
     return {
       ...rules,
-      'TSAbstractMethodDefinition, TSAbstractPropertyDefinition': function (
-        node: never,
-      ): void {
-        if (rules.MethodDefinition) {
-          // for ESLint <= v7
-          rules.MethodDefinition(node)
-        }
-        else if (rules['MethodDefinition, PropertyDefinition']) {
-          // for ESLint >= v8 < v8.3.0
-          rules['MethodDefinition, PropertyDefinition'](node)
-        }
-        else {
-          // for ESLint >= v8.3.0
-          rules['MethodDefinition, PropertyDefinition, StaticBlock']?.(node)
-        }
+      'TSAbstractMethodDefinition, TSAbstractPropertyDefinition': function (node: never): void {
+        rules['MethodDefinition, PropertyDefinition, StaticBlock']?.(node)
       },
     }
   },
