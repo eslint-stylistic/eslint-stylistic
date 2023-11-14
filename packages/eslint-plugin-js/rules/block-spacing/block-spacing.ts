602bfdcdcabd59805fd67362b4c37f979796c23a
auvred <61150013+auvred@users.noreply.github.com>
2023-11-14 17:59:30 +0300

    refactor: migrate block-spacing to ts (#76)

--- packages/eslint-plugin-js/rules/block-spacing/block-spacing.ts
+++ packages/eslint-plugin-js/rules/block-spacing/block-spacing.ts
@@ -3,14 +3,17 @@
  * @author Toru Nagashima
  */
 
+import type { TSESTree } from '@typescript-eslint/utils'
 import { isTokenOnSameLine } from '../../utils/ast-utils'
+import { createRule } from '../../utils/createRule'
+import type { Token } from '../../utils/types'
 
 // ------------------------------------------------------------------------------
 // Rule Definition
 // ------------------------------------------------------------------------------
 
 /** @type {import('eslint').Rule.RuleModule} */
-export default {
+export default createRule({
   meta: {
     type: 'layout',
 
@@ -22,7 +25,7 @@ export default {
     fixable: 'whitespace',
 
     schema: [
-      { enum: ['always', 'never'] },
+      { type: 'string', enum: ['always', 'never'] },
     ],
 
     messages: {
@@ -41,19 +44,19 @@ export default {
      * @param {ASTNode} node A BlockStatement/StaticBlock/SwitchStatement node to get.
      * @returns {Token} The token of the open brace.
      */
-    function getOpenBrace(node) {
+    function getOpenBrace(node: TSESTree.BlockStatement | TSESTree.StaticBlock | TSESTree.SwitchStatement): Token {
       if (node.type === 'SwitchStatement') {
         if (node.cases.length > 0)
-          return sourceCode.getTokenBefore(node.cases[0])
+          return sourceCode.getTokenBefore(node.cases[0])!
 
-        return sourceCode.getLastToken(node, 1)
+        return sourceCode.getLastToken(node, 1)!
       }
 
       if (node.type === 'StaticBlock')
-        return sourceCode.getFirstToken(node, { skip: 1 }) // skip the `static` token
+        return sourceCode.getFirstToken(node, { skip: 1 })! // skip the `static` token
 
       // "BlockStatement"
-      return sourceCode.getFirstToken(node)
+      return sourceCode.getFirstToken(node)!
     }
 
     /**
@@ -67,7 +70,7 @@ export default {
      *    When the option is `"never"`, `true` if there are not any spaces between given tokens.
      *    If given tokens are not on same line, it's always `true`.
      */
-    function isValid(left, right) {
+    function isValid(left: Token, right: Token): boolean {
       return (
         !isTokenOnSameLine(left, right)
                 || sourceCode.isSpaceBetweenTokens(left, right) === always
@@ -79,12 +82,12 @@ export default {
      * @param {ASTNode} node A BlockStatement/StaticBlock/SwitchStatement node to check.
      * @returns {void}
      */
-    function checkSpacingInsideBraces(node) {
+    function checkSpacingInsideBraces(node: TSESTree.BlockStatement | TSESTree.StaticBlock | TSESTree.SwitchStatement): void {
       // Gets braces and the first/last token of content.
       const openBrace = getOpenBrace(node)
-      const closeBrace = sourceCode.getLastToken(node)
-      const firstToken = sourceCode.getTokenAfter(openBrace, { includeComments: true })
-      const lastToken = sourceCode.getTokenBefore(closeBrace, { includeComments: true })
+      const closeBrace = sourceCode.getLastToken(node)!
+      const firstToken = sourceCode.getTokenAfter(openBrace, { includeComments: true })!
+      const lastToken = sourceCode.getTokenBefore(closeBrace, { includeComments: true })!
 
       // Skip if the node is invalid or empty.
       if (openBrace.type !== 'Punctuator'
@@ -159,4 +162,4 @@ export default {
       SwitchStatement: checkSpacingInsideBraces,
     }
   },
-}
+})
