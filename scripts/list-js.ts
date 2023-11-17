3031f4f35f7dda5d096cb08bc0d1d3167dc815d4
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-17 03:50:19 +0100

    chore: remove unused scripts

--- scripts/list-js.ts
+++
@@ -1,9 +0,0 @@
-import fg from 'fast-glob'
-
-const files = fg.sync(['packages/**/*.js'], {
-  ignore: ['**/node_modules/**', '**/dist/**', '**/test-utils/parsers/**'],
-}).sort()
-
-console.log(files.map(i => `./${i}`).join('\n'))
-
-console.log(`\n${files.length} files remaining`)
