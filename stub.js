62a83bb5f07542806edba778bbfe334a18bbd4c8
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-17 19:24:03 +0100

    chore: expose all plugins in stub

--- stub.js
+++ stub.js
@@ -9,11 +9,17 @@ import JITI from 'jiti'
 const jiti = JITI(import.meta.url, {
   alias: {
     '@stylistic/eslint-plugin': fileURLToPath(new URL('./packages/eslint-plugin/src/index.ts', import.meta.url)),
-    '@stylistic/eslint-plugin-js': fileURLToPath(new URL('./packages/eslint-plugin-js/src/index.js', import.meta.url)),
-    '@stylistic/eslint-plugin-jsx': fileURLToPath(new URL('./packages/eslint-plugin-jsx/src/index.js', import.meta.url)),
+    '@stylistic/eslint-plugin-js': fileURLToPath(new URL('./packages/eslint-plugin-js/src/index.ts', import.meta.url)),
+    '@stylistic/eslint-plugin-jsx': fileURLToPath(new URL('./packages/eslint-plugin-jsx/src/index.ts', import.meta.url)),
     '@stylistic/eslint-plugin-ts': fileURLToPath(new URL('./packages/eslint-plugin-ts/src/index.ts', import.meta.url)),
     '@eslint-stylistic/metadata': fileURLToPath(new URL('./packages/metadata/src/index.ts', import.meta.url)),
   },
 })
 
-export default jiti('@stylistic/eslint-plugin').default
+export const pluginDefault = jiti('@stylistic/eslint-plugin').default
+export const pluginJS = jiti('@stylistic/eslint-plugin-js').default
+export const pluginJSX = jiti('@stylistic/eslint-plugin-jsx').default
+export const pluginTS = jiti('@stylistic/eslint-plugin-ts').default
+export const metadata = jiti('@eslint-stylistic/metadata').default
+
+export default pluginDefault
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ export default {
   resolve: {
     alias: {
       '@stylistic/eslint-plugin': fileURLToPath(new URL('./packages/eslint-plugin/src/index.ts', import.meta.url)),
-      '@stylistic/eslint-plugin-js': fileURLToPath(new URL('./packages/eslint-plugin-js/src/index.js', import.meta.url)),
-      '@stylistic/eslint-plugin-jsx': fileURLToPath(new URL('./packages/eslint-plugin-jsx/src/index.js', import.meta.url)),
+      '@stylistic/eslint-plugin-js': fileURLToPath(new URL('./packages/eslint-plugin-js/src/index.ts', import.meta.url)),
+      '@stylistic/eslint-plugin-jsx': fileURLToPath(new URL('./packages/eslint-plugin-jsx/src/index.ts', import.meta.url)),
       '@stylistic/eslint-plugin-ts': fileURLToPath(new URL('./packages/eslint-plugin-ts/src/index.ts', import.meta.url)),
       '@eslint-stylistic/metadata': fileURLToPath(new URL('./packages/metadata/src/index.ts', import.meta.url)),
     },
