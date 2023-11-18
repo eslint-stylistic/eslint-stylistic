bb294a636a65fc24011176eb36d2dd6ab4e17e3c
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-18 11:37:47 +0100

    chore: update

--- packages/eslint-plugin-plus/configs/index.ts
+++
@@ -1,7 +0,0 @@
-import { createAllConfigs } from '../../shared/configs-all'
-import plugin from '../src/plugin'
-
-export const configs = {
-  'all-flat': createAllConfigs(plugin, '@stylistic/ts', true),
-  'all-extends': createAllConfigs(plugin, '@stylistic/ts', false),
-}
