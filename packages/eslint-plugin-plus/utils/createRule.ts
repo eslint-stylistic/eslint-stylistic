b4bd6828106411f06d4fb5f443598483e37c23d0
Roman Nuritdinov <github@woof.slmail.me>
2023-12-11 23:19:48 +0200

    docs: update rules links for "plus" plugin (#221)

--- packages/eslint-plugin-plus/utils/createRule.ts
+++ packages/eslint-plugin-plus/utils/createRule.ts
@@ -1,5 +1,5 @@
 import { ESLintUtils } from '@typescript-eslint/utils'
 
 export const createRule = ESLintUtils.RuleCreator(
-  name => `https://eslint.style/rules/ts/${name}`,
+  name => `https://eslint.style/rules/plus/${name}`,
 )
