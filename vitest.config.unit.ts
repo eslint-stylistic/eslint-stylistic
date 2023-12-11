82efdd52372930b854ab3b0227b639db456179c5
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-11 12:00:36 +0100

    chore: try fix coverage

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import viteConfig from './vite.config.js'
 
 export default defineConfig({
+  ...viteConfig,
   test: {
     root: './packages',
     globals: true,
