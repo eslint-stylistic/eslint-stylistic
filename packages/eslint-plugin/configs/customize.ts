8dedf195ad71fee0a71ca250571024641a55b46e
Seth Murphy <seth.murphy@garavest.com>
2024-01-01 00:33:04 -0600

    docs: updated JSDoc comments to reflect Prettier options

--- packages/eslint-plugin/configs/customize.ts
+++ packages/eslint-plugin/configs/customize.ts
@@ -17,16 +17,22 @@ export interface StylisticCustomizeOptions<Flat extends boolean = true> {
   pluginName?: string
   /**
    * Indentation level
+   * Similar to the `tabWidth` and `useTabs` options in Prettier
+   *
    * @default 2
    */
   indent?: number | 'tab'
   /**
    * Quote style
+   * Similar to `singleQuote` option in Prettier
+   *
    * @default 'single'
    */
   quotes?: 'single' | 'double'
   /**
    * Whether to enable semicolons
+   * Similar to `semi` option in Prettier
+   *
    * @default false
    */
   semi?: boolean
@@ -37,6 +43,8 @@ export interface StylisticCustomizeOptions<Flat extends boolean = true> {
   jsx?: boolean
   /**
    * When to enable arrow parenthesis
+   * Similar to `arrowParens` option in Prettier
+   *
    * @default false
    */
   arrowParens?: boolean
@@ -47,16 +55,22 @@ export interface StylisticCustomizeOptions<Flat extends boolean = true> {
   braceStyle?: '1tbs' | 'stroustrup' | 'allman'
   /**
    * Whether to require spaces around braces
+   * Similar to `bracketSpacing` option in Prettier
+   *
    * @default true
    */
   blockSpacing?: boolean
   /**
    * When to enable prop quoting
+   * Similar to `quoteProps` option in Prettier
+   *
    * @default 'consistent-as-needed'
    */
   quoteProps?: 'always' | 'as-needed' | 'consistent' | 'consistent-as-needed'
   /**
    * When to enable comma dangles
+   * Similar to `trailingComma` option in Prettier
+   *
    * @default 'always-multiline'
    */
   commaDangle?: 'never' | 'always' | 'always-multiline' | 'only-multiline'
