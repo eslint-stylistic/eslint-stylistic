f7bc3a98174db964890206f74cb5594315bf3efb
Justin McBride <jstnmcbrd@outlook.com>
2024-02-17 23:12:57 -0800

    fix: type error on `UnprefixedRuleOptions` (#284)
    * Fix UnprefixedRuleOptions type extension
    
    * Fix lint error

--- packages/eslint-plugin/dts/rule-options.d.ts
+++ packages/eslint-plugin/dts/rule-options.d.ts
@@ -3,7 +3,7 @@ import type { UnprefixedRuleOptions as JSX } from '@stylistic/eslint-plugin-jsx'
 import type { UnprefixedRuleOptions as TS } from '@stylistic/eslint-plugin-ts'
 import type { UnprefixedRuleOptions as PLUS } from '@stylistic/eslint-plugin-plus'
 
-export interface UnprefixedRuleOptions extends JS, JSX, TS, PLUS {}
+export type UnprefixedRuleOptions = JS & JSX & TS & PLUS
 
 export type RuleOptions = {
   [K in keyof UnprefixedRuleOptions as `@stylistic/${K}`]: UnprefixedRuleOptions[K]
