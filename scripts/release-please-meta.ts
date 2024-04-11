ab8079c68a2f9f3986d2ed561fe1a5af313bad0a
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-11 16:42:45 +0200

    chore: try fix release-please action

--- scripts/release-please-meta.ts
+++ scripts/release-please-meta.ts
@@ -1,14 +1,13 @@
 import fs from 'node:fs/promises'
 import fg from 'fast-glob'
-import { dump, load } from 'js-yaml'
 
 const files = await fg('packages/**/package.json', {
   ignore: ['**/node_modules/**', '**/dist/**', '**/coverage/**'],
 })
 
-const json: any = load(await fs.readFile('.github/workflows/release-please.yml', 'utf8'))
+const json = JSON.parse(await fs.readFile('release-please-config.json', 'utf8'))
 
-json.jobs['release-please'].steps[0].with['extra-files'] = JSON.stringify(files
+json['extra-files'] = files
   .sort()
   .map((file) => {
     return {
@@ -16,6 +15,6 @@ json.jobs['release-please'].steps[0].with['extra-files'] = JSON.stringify(files
       path: file,
       jsonpath: '$.version',
     }
-  }), null, 2)
+  })
 
-await fs.writeFile('.github/workflows/release-please.yml', dump(json), 'utf-8')
+await fs.writeFile('release-please-config.json', `${JSON.stringify(json, null, 2)}\n`, 'utf-8')
