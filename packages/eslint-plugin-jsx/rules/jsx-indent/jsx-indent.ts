e844e405dd41694c15cf9c6f2f15d16c33bba2f0
Anthony Fu <github@antfu.me>
2024-04-29 15:46:35 +0200

    Update packages/eslint-plugin-jsx/rules/jsx-indent/jsx-indent.ts

--- packages/eslint-plugin-jsx/rules/jsx-indent/jsx-indent.ts
+++ packages/eslint-plugin-jsx/rules/jsx-indent/jsx-indent.ts
@@ -380,6 +380,7 @@ export default createRule<MessageIds, RuleOptions>({
       const nameIndent = getNodeIndent(node.name)
       const lastToken = context.sourceCode.getLastToken(node.value)!
       const firstInLine = getFirstNodeInLine(context, lastToken)
+      // leave this for `indent` rule to handle
       if (firstInLine.loc.start.line !== lastToken.loc.start.line)
         return
       const indent = node.name.loc.start.line === firstInLine.loc.start.line ? 0 : nameIndent
