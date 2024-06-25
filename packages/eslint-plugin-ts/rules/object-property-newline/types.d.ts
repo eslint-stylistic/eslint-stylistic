036d3de3a02e86dde21b01369f98521571cd4273
Vida Xie <vida_2020@163.com>
2024-06-25 22:19:14 +0800

    feat(ts): add `object-curly-newline` and `object-property-newline` (#444)
    * feat(ts/object-property-newline): add ts-version of `object-property-newline`
    
    * feat(ts/object-curly-newline): add ts-version of `object-curly-newline`
    
    * chore(object-property-newline): update test
    
    * chore: run update

---
+++ packages/eslint-plugin-ts/rules/object-property-newline/types.d.ts
@@ -0,0 +1,9 @@
+/* GENERATED, DO NOT EDIT DIRECTLY */
+
+export interface Schema0 {
+  allowAllPropertiesOnSameLine?: boolean
+  allowMultiplePropertiesPerLine?: boolean
+}
+
+export type RuleOptions = [Schema0?]
+export type MessageIds = 'propertiesOnNewlineAll' | 'propertiesOnNewline'
