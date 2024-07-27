d5ae88d6602f0e506872d06ef3dd1b4e6443638e
Stephen Zhou <hi@hyoban.cc>
2024-07-28 04:24:42 +0800

    fix(indent): handle mixed spaces and tabs (#465)

--- packages/eslint-plugin-js/rules/indent/indent.ts
+++ packages/eslint-plugin-js/rules/indent/indent.ts
@@ -755,8 +755,6 @@ export default createRule<MessageIds, RuleOptions>({
       const indentation = tokenInfo.getTokenIndent(token)
 
       return indentation === desiredIndent
-        // To avoid conflicts with no-mixed-spaces-and-tabs, don't report mixed spaces and tabs.
-        || indentation.includes(' ') && indentation.includes('\t')
     }
 
     /**
