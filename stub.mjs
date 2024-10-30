2d62b99d0a335d3235de2d99802c056f3453b539
Anthony Fu <github@antfu.me>
2024-10-31 04:48:15 +0900

    chore: update deps

--- stub.mjs
+++ stub.mjs
@@ -1,18 +1,19 @@
+/* eslint-disable antfu/no-top-level-await */
 /**
  * Stub file for `@stylistic/eslint-plugin`
  * Importing this file will directly load from source,
  * so no build process is needed, and it always stays up-to-date.
  */
-import JITI from 'jiti'
+import { createJiti } from 'jiti'
 import { alias } from './alias.mjs'
 
-const jiti = JITI(import.meta.url, { alias })
+const jiti = createJiti(import.meta.url, { alias })
 
-export const pluginDefault = jiti('@stylistic/eslint-plugin').default
-export const pluginJS = jiti('@stylistic/eslint-plugin-js').default
-export const pluginJSX = jiti('@stylistic/eslint-plugin-jsx').default
-export const pluginTS = jiti('@stylistic/eslint-plugin-ts').default
-export const pluginPlus = jiti('@stylistic/eslint-plugin-plus').default
-export const metadata = jiti('@eslint-stylistic/metadata').default
+export const pluginDefault = await jiti.import('@stylistic/eslint-plugin').then(m => m.default)
+export const pluginJS = await jiti.import('@stylistic/eslint-plugin-js').then(m => m.default)
+export const pluginJSX = await jiti.import('@stylistic/eslint-plugin-jsx').then(m => m.default)
+export const pluginTS = await jiti.import('@stylistic/eslint-plugin-ts').then(m => m.default)
+export const pluginPlus = await jiti.import('@stylistic/eslint-plugin-plus').then(m => m.default)
+export const metadata = await jiti.import('@eslint-stylistic/metadata').then(m => m.default)
 
 export default pluginDefault
