f759b0c48012908bbed38e410630d6dacc066b94
Vida Xie <vida_2020@163.com>
2025-03-09 01:39:47 +0800

    refactor: align ts nodes to js

--- packages/eslint-plugin/rules/comma-dangle/comma-dangle._ts_.ts
+++ packages/eslint-plugin/rules/comma-dangle/comma-dangle._ts_.ts
@@ -1,5 +1,7 @@
-import type { ASTNode, Tree } from '#types'
+import type { Tree } from '#types'
+import type { Value } from './types'
 import type { MessageIds, RuleOptions } from './types._ts_'
+import { getNextLocation } from '#utils/ast'
 import { castRuleModule, createRule } from '#utils/create-rule'
 import { AST_NODE_TYPES } from '@typescript-eslint/utils'
 import { isCommaToken } from '@typescript-eslint/utils/ast-utils'
@@ -21,6 +23,50 @@ const OPTION_VALUE_SCHEME = [
 ]
 
 const DEFAULT_OPTION_VALUE = 'never'
+
+const closeBraces = ['}', ']', ')', '>']
+
+type TargetASTNode =
+  | Tree.ArrayExpression
+  | Tree.ArrayPattern
+  | Tree.ObjectExpression
+  | Tree.ObjectPattern
+  | Tree.ImportDeclaration
+  | Tree.ExportNamedDeclaration
+  | Tree.FunctionDeclaration
+  | Tree.FunctionExpression
+  | Tree.ArrowFunctionExpression
+  | Tree.CallExpression
+  | Tree.NewExpression
+  | Tree.ImportExpression
+  | Tree.ExportAllDeclaration
+  | Tree.TSEnumDeclaration
+  | Tree.TSTypeParameterDeclaration
+  | Tree.TSTupleType
+
+type ItemASTNode = NonNullable<
+  | Tree.ObjectLiteralElement
+  | Tree.ObjectPattern['properties'][number]
+  | Tree.ArrayExpression['elements'][number]
+  | Tree.ArrayPattern['elements'][number]
+  | Tree.ImportClause
+  | Tree.ExportSpecifier
+  | Tree.Parameter
+  | Tree.ImportAttribute
+  | Tree.TSEnumMember
+  | Tree.TSTypeParameter
+  | Tree.TypeNode
+>
+
+/**
+ * Checks whether or not a trailing comma is allowed in a given node.
+ * If the `lastItem` is `RestElement` or `RestProperty`, it disallows trailing commas.
+ * @param lastItem The node of the last element in the given node.
+ * @returns `true` if a trailing comma is allowed.
+ */
+function isTrailingCommaAllowed(lastItem: ItemASTNode) {
+  return lastItem.type !== 'RestElement'
+}
 
 function normalizeOptions(options: Option = {}): NormalizedOptions {
   if (typeof options === 'string') {
@@ -96,7 +142,14 @@ export default createRule<RuleOptions, MessageIds>({
     const isTSX = context.parserOptions?.ecmaFeatures?.jsx
       && context.filename?.endsWith('.tsx')
 
-    const predicate = {
+    interface VerifyInfo {
+      /** A node to check. */
+      node: TargetASTNode
+      /** The last item of the node. */
+      lastItem: ItemASTNode | null
+    }
+
+    const predicate: Record<Value | 'ignore' | string, (info: VerifyInfo) => void> = {
       'always': forceComma,
       'always-multiline': forceCommaIfMultiline,
       'only-multiline': allowCommaIfMultiline,
@@ -105,36 +158,68 @@ export default createRule<RuleOptions, MessageIds>({
       'ignore': () => {},
     }
 
-    function last(nodes: ASTNode[]): ASTNode | null {
+    function last<T>(nodes: T[] | undefined): T | null {
+      if (!nodes)
+        return null
       return nodes[nodes.length - 1] ?? null
     }
 
-    function getLastItem(node: ASTNode): ASTNode | null {
-      switch (node.type) {
-        case AST_NODE_TYPES.TSEnumDeclaration:
-          return last(node.body?.members || node.members)
-        case AST_NODE_TYPES.TSTypeParameterDeclaration:
-          return last(node.params)
-        case AST_NODE_TYPES.TSTupleType:
-          return last(node.elementTypes)
-        default:
-          return null
+    /**
+     * Gets the trailing comma token of the given node.
+     * If the trailing comma does not exist, this returns the token which is
+     * the insertion point of the trailing comma token.
+     * @param info The information to verify.
+     * @returns The trailing comma token or the insertion point.
+     */
+    function getTrailingToken(info: VerifyInfo) {
+      switch (info.node.type) {
+        case 'ObjectExpression':
+        case 'ArrayExpression':
+        case 'CallExpression':
+        case 'NewExpression':
+        case 'ImportExpression':
+          return sourceCode.getLastToken(info.node, 1)
+        default: {
+          const lastItem = info.lastItem
+          if (!lastItem)
+            return null
+          const nextToken = sourceCode.getTokenAfter(lastItem)!
+
+          if (isCommaToken(nextToken))
+            return nextToken
+
+          return sourceCode.getLastToken(lastItem)
+        }
       }
     }
 
-    function getTrailingToken(node: ASTNode): Tree.Token | null {
-      const last = getLastItem(node)
-      const trailing = last && sourceCode.getTokenAfter(last)
-      return trailing
+    /**
+     * Checks whether or not a given node is multiline.
+     * This rule handles a given node as multiline when the closing parenthesis
+     * and the last element are not on the same line.
+     * @param info The information to verify.
+     * @returns `true` if the node is multiline.
+     */
+    function isMultiline(info: VerifyInfo) {
+      const lastItem = info.lastItem
+
+      if (!lastItem)
+        return false
+      const penultimateToken = getTrailingToken(info)
+      if (!penultimateToken)
+        return false
+      const lastToken = sourceCode.getTokenAfter(penultimateToken)
+      if (!lastToken)
+        return false
+
+      return lastToken.loc.end.line !== penultimateToken.loc.end.line
     }
 
-    function isMultiline(node: ASTNode): boolean {
-      const last = getLastItem(node)
-      const lastToken = sourceCode.getLastToken(node)
-      return last?.loc.end.line !== lastToken?.loc.end.line
-    }
-
-    function forbidComma(node: ASTNode): void {
+    /**
+     * Reports a trailing comma if it exists.
+     * @param info The information to verify.
+     */
+    function forbidComma(info: VerifyInfo): void {
       /**
        * We allow tailing comma in TSTypeParameterDeclaration in TSX,
        * because it's used to differentiate JSX tags from generics.
@@ -142,56 +227,133 @@ export default createRule<RuleOptions, MessageIds>({
        * https://github.com/microsoft/TypeScript/issues/15713#issuecomment-499474386
        * https://github.com/eslint-stylistic/eslint-stylistic/issues/35
        */
-      if (isTSX && node.type === AST_NODE_TYPES.TSTypeParameterDeclaration && node.params.length === 1)
+      if (isTSX && info.node.type === AST_NODE_TYPES.TSTypeParameterDeclaration && info.node.params.length === 1)
         return
 
-      /**
-       * Forbid tailing comma
-       */
-      const last = getLastItem(node)
-      const trailing = getTrailingToken(node)
-      if (last && trailing && isCommaToken(trailing)) {
+      const lastItem = info.lastItem
+
+      if (!lastItem)
+        return
+
+      const trailingToken = getTrailingToken(info)
+
+      if (trailingToken && isCommaToken(trailingToken)) {
         context.report({
-          node,
+          node: lastItem,
+          loc: trailingToken.loc,
           messageId: 'unexpected',
-          fix(fixer) {
-            return fixer.remove(trailing)
+          * fix(fixer) {
+            yield fixer.remove(trailingToken)
+
+            /**
+             * Extend the range of the fix to include surrounding tokens to ensure
+             * that the element after which the comma is removed stays _last_.
+             * This intentionally makes conflicts in fix ranges with rules that may be
+             * adding or removing elements in the same autofix pass.
+             * https://github.com/eslint/eslint/issues/15660
+             */
+            yield fixer.insertTextBefore(sourceCode.getTokenBefore(trailingToken)!, '')
+            yield fixer.insertTextAfter(sourceCode.getTokenAfter(trailingToken)!, '')
           },
         })
       }
     }
 
-    function forceComma(node: ASTNode): void {
-      const last = getLastItem(node)
-      const trailing = getTrailingToken(node)
-      if (last && trailing && !isCommaToken(trailing)) {
-        context.report({
-          node,
-          messageId: 'missing',
-          fix(fixer) {
-            return fixer.insertTextAfter(last, ',')
-          },
-        })
+    /**
+     * Reports the last element of a given node if it does not have a trailing
+     * comma.
+     *
+     * If a given node is `ArrayPattern` which has `RestElement`, the trailing
+     * comma is disallowed, so report if it exists.
+     * @param info The information to verify.
+     */
+    function forceComma(info: VerifyInfo): void {
+      const lastItem = info.lastItem
+
+      if (!lastItem)
+        return
+
+      if (!isTrailingCommaAllowed(lastItem)) {
+        forbidComma(info)
+        return
       }
+
+      const trailingToken = getTrailingToken(info)
+
+      if (!trailingToken || trailingToken.value === ',')
+        return
+
+      const nextToken = sourceCode.getTokenAfter(trailingToken)
+      if (!nextToken || !closeBraces.includes(nextToken.value))
+        return
+
+      context.report({
+        node: lastItem,
+        loc: {
+          start: trailingToken.loc.end,
+          end: getNextLocation(sourceCode, trailingToken.loc.end)!,
+        },
+        messageId: 'missing',
+        * fix(fixer) {
+          yield fixer.insertTextAfter(trailingToken, ',')
+
+          /**
+           * Extend the range of the fix to include surrounding tokens to ensure
+           * that the element after which the comma is inserted stays _last_.
+           * This intentionally makes conflicts in fix ranges with rules that may be
+           * adding or removing elements in the same autofix pass.
+           * https://github.com/eslint/eslint/issues/15660
+           */
+          yield fixer.insertTextBefore(trailingToken, '')
+          yield fixer.insertTextAfter(sourceCode.getTokenAfter(trailingToken)!, '')
+        },
+      })
     }
 
-    function allowCommaIfMultiline(node: ASTNode): void {
-      if (!isMultiline(node))
-        forbidComma(node)
+    /**
+     * Only if a given node is not multiline, reports the last element of a given node
+     * when it does not have a trailing comma.
+     * Otherwise, reports a trailing comma if it exists.
+     * @param info The information to verify.
+     */
+    function allowCommaIfMultiline(info: VerifyInfo) {
+      if (!isMultiline(info))
+        forbidComma(info)
     }
 
-    function forceCommaIfMultiline(node: ASTNode): void {
-      if (isMultiline(node))
-        forceComma(node)
+    /**
+     * If a given node is multiline, reports the last element of a given node
+     * when it does not have a trailing comma.
+     * Otherwise, reports a trailing comma if it exists.
+     * @param info The information to verify.
+     */
+    function forceCommaIfMultiline(info: VerifyInfo) {
+      if (isMultiline(info))
+        forceComma(info)
       else
-        forbidComma(node)
+        forbidComma(info)
     }
 
     return {
       ...rules,
-      TSEnumDeclaration: predicate[normalizedOptions.enums],
-      TSTypeParameterDeclaration: predicate[normalizedOptions.generics],
-      TSTupleType: predicate[normalizedOptions.tuples],
+      TSEnumDeclaration(node) {
+        predicate[normalizedOptions.enums]({
+          node,
+          lastItem: last(node.body?.members ?? node.members),
+        })
+      },
+      TSTypeParameterDeclaration(node) {
+        predicate[normalizedOptions.generics]({
+          node,
+          lastItem: last(node.params),
+        })
+      },
+      TSTupleType(node) {
+        predicate[normalizedOptions.tuples]({
+          node,
+          lastItem: last(node.elementTypes),
+        })
+      },
     }
   },
 })
