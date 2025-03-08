f1b6246c526432b393bef7b85502348e6694ba94
Vida Xie <vida_2020@163.com>
2025-03-09 02:51:11 +0800

    refactor: align to js version

--- packages/eslint-plugin/rules/object-property-newline/object-property-newline._ts_.ts
+++ packages/eslint-plugin/rules/object-property-newline/object-property-newline._ts_.ts
@@ -1,4 +1,4 @@
-import type { Tree } from '#types'
+import type { ASTNode, Tree } from '#types'
 import type { MessageIds, RuleOptions } from './types'
 import { castRuleModule, createRule } from '#utils/create-rule'
 import _baseRule from './object-property-newline._js_'
@@ -22,7 +22,6 @@ export default createRule<RuleOptions, MessageIds>({
   ],
 
   create(context) {
-    const rules = baseRule.create(context)
     const allowSameLine = context.options[0] && (
       (context.options[0].allowAllPropertiesOnSameLine || context.options[0].allowMultiplePropertiesPerLine /* Deprecated */)
     )
@@ -32,56 +31,52 @@ export default createRule<RuleOptions, MessageIds>({
 
     const sourceCode = context.sourceCode
 
+    function check(node: ASTNode, children: Tree.ObjectLiteralElement[] | Tree.TypeElement[]) {
+      if (allowSameLine) {
+        if (children.length > 1) {
+          const firstTokenOfFirstProperty = sourceCode.getFirstToken(children[0])!
+          const lastTokenOfLastProperty = sourceCode.getLastToken(children[children.length - 1])!
+
+          if (firstTokenOfFirstProperty.loc.end.line === lastTokenOfLastProperty.loc.start.line) {
+            // All keys and values are on the same line
+            return
+          }
+        }
+      }
+
+      for (let i = 1; i < children.length; i++) {
+        const lastTokenOfPreviousProperty = sourceCode.getLastToken(children[i - 1])!
+        const firstTokenOfCurrentProperty = sourceCode.getFirstToken(children[i])!
+
+        if (lastTokenOfPreviousProperty.loc.end.line === firstTokenOfCurrentProperty.loc.start.line) {
+          context.report({
+            node,
+            loc: firstTokenOfCurrentProperty.loc,
+            messageId,
+            fix(fixer) {
+              const comma = sourceCode.getTokenBefore(firstTokenOfCurrentProperty)!
+              const rangeAfterComma = [comma.range[1], firstTokenOfCurrentProperty.range[0]] as const
+
+              // Don't perform a fix if there are any comments between the comma and the next property.
+              if (sourceCode.text.slice(rangeAfterComma[0], rangeAfterComma[1]).trim())
+                return null
+
+              return fixer.replaceTextRange(rangeAfterComma, '\n')
+            },
+          })
+        }
+      }
+    }
+
     return {
       ObjectExpression(node) {
-        if (allowSameLine) {
-          if (node.properties.length > 1) {
-            const firstTokenOfFirstProperty = sourceCode.getFirstToken(node.properties[0])!
-            const lastTokenOfLastProperty = sourceCode.getLastToken(node.properties[node.properties.length - 1])!
-
-            if (firstTokenOfFirstProperty.loc.end.line === lastTokenOfLastProperty.loc.start.line) {
-              // All keys and values are on the same line
-              return
-            }
-          }
-        }
-
-        for (let i = 1; i < node.properties.length; i++) {
-          const lastTokenOfPreviousProperty = sourceCode.getLastToken(node.properties[i - 1])!
-          const firstTokenOfCurrentProperty = sourceCode.getFirstToken(node.properties[i])!
-
-          if (lastTokenOfPreviousProperty.loc.end.line === firstTokenOfCurrentProperty.loc.start.line) {
-            context.report({
-              node,
-              loc: firstTokenOfCurrentProperty.loc,
-              messageId,
-              fix(fixer) {
-                const comma = sourceCode.getTokenBefore(firstTokenOfCurrentProperty)!
-                const rangeAfterComma = [comma.range[1], firstTokenOfCurrentProperty.range[0]] as const
-
-                // Don't perform a fix if there are any comments between the comma and the next property.
-                if (sourceCode.text.slice(rangeAfterComma[0], rangeAfterComma[1]).trim())
-                  return null
-
-                return fixer.replaceTextRange(rangeAfterComma, '\n')
-              },
-            })
-          }
-        }
+        check(node, node.properties)
       },
-      TSTypeLiteral(node: Tree.TSTypeLiteral) {
-        return rules.ObjectExpression!({
-          ...node,
-          // @ts-expect-error only used to get token and loc
-          properties: node.members,
-        })
+      TSTypeLiteral(node) {
+        check(node, node.members)
       },
-      TSInterfaceBody(node: Tree.TSInterfaceBody) {
-        return rules.ObjectExpression!({
-          ...node,
-          // @ts-expect-error only used to get token and loc
-          properties: node.body,
-        })
+      TSInterfaceBody(node) {
+        check(node, node.body)
       },
     }
   },
