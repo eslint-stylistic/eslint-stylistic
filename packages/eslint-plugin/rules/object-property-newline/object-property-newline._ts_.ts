d49ef91e765938c60e2b344391a0d5a09c85bdc1
Vida Xie <vida_2020@163.com>
2025-03-09 09:25:39 +0800

    refactor: merge `object-property-newline` (#719)
    * refactor: copy from js
    
    * refactor: align to js version
    
    * chore: complete merge

--- packages/eslint-plugin/rules/object-property-newline/object-property-newline._ts_.ts
+++ packages/eslint-plugin/rules/object-property-newline/object-property-newline._ts_.ts
@@ -1,17 +1,35 @@
-import type { Tree } from '#types'
+import type { ASTNode, Tree } from '#types'
 import type { MessageIds, RuleOptions } from './types'
-import { castRuleModule, createRule } from '#utils/create-rule'
-import _baseRule from './object-property-newline._js_'
-
-const baseRule = /* @__PURE__ */ castRuleModule(_baseRule)
+import { createRule } from '#utils/create-rule'
 
 export default createRule<RuleOptions, MessageIds>({
   name: 'object-property-newline',
   package: 'ts',
   meta: {
-    ...baseRule.meta,
+    type: 'layout',
     docs: {
       description: 'Enforce placing object properties on separate lines',
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          allowAllPropertiesOnSameLine: {
+            type: 'boolean',
+            default: false,
+          },
+          allowMultiplePropertiesPerLine: { // Deprecated
+            type: 'boolean',
+            default: false,
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+    fixable: 'whitespace',
+    messages: {
+      propertiesOnNewlineAll: 'Object properties must go on a new line if they aren\'t all on the same line.',
+      propertiesOnNewline: 'Object properties must go on a new line.',
     },
   },
   defaultOptions: [
@@ -22,23 +40,61 @@ export default createRule<RuleOptions, MessageIds>({
   ],
 
   create(context) {
-    const rules = baseRule.create(context)
+    const allowSameLine = context.options[0] && (
+      (context.options[0].allowAllPropertiesOnSameLine || context.options[0].allowMultiplePropertiesPerLine /* Deprecated */)
+    )
+    const messageId = allowSameLine
+      ? 'propertiesOnNewlineAll'
+      : 'propertiesOnNewline'
+
+    const sourceCode = context.sourceCode
+
+    function check(node: ASTNode, children: Tree.ObjectLiteralElement[] | Tree.TypeElement[]) {
+      if (allowSameLine) {
+        if (children.length > 1) {
+          const firstTokenOfFirstProperty = sourceCode.getFirstToken(children[0])!
+          const lastTokenOfLastProperty = sourceCode.getLastToken(children[children.length - 1])!
+
+          if (firstTokenOfFirstProperty.loc.end.line === lastTokenOfLastProperty.loc.start.line) {
+            // All keys and values are on the same line
+            return
+          }
+        }
+      }
+
+      for (let i = 1; i < children.length; i++) {
+        const lastTokenOfPreviousProperty = sourceCode.getLastToken(children[i - 1])!
+        const firstTokenOfCurrentProperty = sourceCode.getFirstToken(children[i])!
+
+        if (lastTokenOfPreviousProperty.loc.end.line === firstTokenOfCurrentProperty.loc.start.line) {
+          context.report({
+            node,
+            loc: firstTokenOfCurrentProperty.loc,
+            messageId,
+            fix(fixer) {
+              const comma = sourceCode.getTokenBefore(firstTokenOfCurrentProperty)!
+              const rangeAfterComma = [comma.range[1], firstTokenOfCurrentProperty.range[0]] as const
+
+              // Don't perform a fix if there are any comments between the comma and the next property.
+              if (sourceCode.text.slice(rangeAfterComma[0], rangeAfterComma[1]).trim())
+                return null
+
+              return fixer.replaceTextRange(rangeAfterComma, '\n')
+            },
+          })
+        }
+      }
+    }
 
     return {
-      ...rules,
-      TSTypeLiteral(node: Tree.TSTypeLiteral) {
-        return rules.ObjectExpression!({
-          ...node,
-          // @ts-expect-error only used to get token and loc
-          properties: node.members,
-        })
+      ObjectExpression(node) {
+        check(node, node.properties)
       },
-      TSInterfaceBody(node: Tree.TSInterfaceBody) {
-        return rules.ObjectExpression!({
-          ...node,
-          // @ts-expect-error only used to get token and loc
-          properties: node.body,
-        })
+      TSTypeLiteral(node) {
+        check(node, node.members)
+      },
+      TSInterfaceBody(node) {
+        check(node, node.body)
       },
     }
   },
