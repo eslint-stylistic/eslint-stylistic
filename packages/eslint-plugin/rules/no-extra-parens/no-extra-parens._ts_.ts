7882a093b4c1ff131aa291783f3834b8eeccf663
mori <ys0514@yeah.net>
2025-03-24 21:00:27 +0800

    add comments

--- packages/eslint-plugin/rules/no-extra-parens/no-extra-parens._ts_.ts
+++ packages/eslint-plugin/rules/no-extra-parens/no-extra-parens._ts_.ts
@@ -1351,6 +1351,7 @@ export default createRule<RuleOptions, MessageIds>({
           // RegExp literal is allowed to have parens (https://github.com/eslint/eslint/issues/1589)
           && !(node.argument.type === 'Literal' && 'regex' in node.argument && node.argument.regex)
         ) {
+          // When multiple lines are returned and the first line contains only one '(', do not report, otherwise it will destroy the program
           const returnNode = node.argument
           const returnLine = sourceCode.getText(node).split('\n')[0]
           if ((returnLine.match(/\(/g) || []).length === 1 && returnNode.loc.start.line !== returnNode.loc.end.line)
