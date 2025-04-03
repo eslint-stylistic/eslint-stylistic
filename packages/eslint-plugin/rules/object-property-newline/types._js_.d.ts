196744e661a8e4334701492621eabd2ce0211135
Vida Xie <vida_2020@163.com>
2025-04-03 14:23:28 +0800

    feat: revert changes in js

--- packages/eslint-plugin/rules/object-property-newline/types._js_.d.ts
+++ packages/eslint-plugin/rules/object-property-newline/types._js_.d.ts
@@ -1,46 +1,10 @@
 /* GENERATED, DO NOT EDIT DIRECTLY */
 
-/* @checksum: lIcu3uwAmu */
+/* @checksum: OYSGLfA723 */
 
 export interface ObjectPropertyNewlineSchema0 {
   allowAllPropertiesOnSameLine?: boolean
   allowMultiplePropertiesPerLine?: boolean
-  ObjectExpression?:
-    | boolean
-    | {
-      allowAllPropertiesOnSameLine?: boolean
-      allowMultiplePropertiesPerLine?: boolean
-    }
-  ObjectPattern?:
-    | boolean
-    | {
-      allowAllPropertiesOnSameLine?: boolean
-      allowMultiplePropertiesPerLine?: boolean
-    }
-  ImportDeclaration?:
-    | boolean
-    | {
-      allowAllPropertiesOnSameLine?: boolean
-      allowMultiplePropertiesPerLine?: boolean
-    }
-  ExportDeclaration?:
-    | boolean
-    | {
-      allowAllPropertiesOnSameLine?: boolean
-      allowMultiplePropertiesPerLine?: boolean
-    }
-  TSTypeLiteral?:
-    | boolean
-    | {
-      allowAllPropertiesOnSameLine?: boolean
-      allowMultiplePropertiesPerLine?: boolean
-    }
-  TSInterfaceBody?:
-    | boolean
-    | {
-      allowAllPropertiesOnSameLine?: boolean
-      allowMultiplePropertiesPerLine?: boolean
-    }
 }
 
 export type ObjectPropertyNewlineRuleOptions = [
