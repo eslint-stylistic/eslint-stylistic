001851960ad5cf7e2490aa097655202dde2a80ba
Vida Xie <vida_2020@163.com>
2025-05-24 10:50:39 +0800

    refactor: merge `keyword-spacing` (#732)

--- packages/eslint-plugin/rules/keyword-spacing/keyword-spacing._js_.ts
+++ packages/eslint-plugin/rules/keyword-spacing/keyword-spacing._js_.ts
@@ -3,6 +3,9 @@
  * @author Toru Nagashima
  */
 
+// MERGED: The JS version of this rule is merged to the TS version, this file will be removed
+// in the next major when we remove the `@stylistic/eslint-plugin-js` package.
+
 import type { ASTNode, JSONSchema, Token, Tree } from '#types'
 import type { MessageIds, RuleOptions } from './types._js_'
 import { isKeywordToken, isNotOpeningParenToken, isTokenOnSameLine } from '#utils/ast'
