c18efb0f22c7b06f0be52b1aaa5afacf24a8ac12
Yosuke Ota <otameshiyo23@gmail.com>
2025-06-20 10:05:20 +0900

    feat: no longer deps on `typescript` (#838)
    Co-authored-by: Vida Xie <vida_2020@163.com>
    Co-authored-by: Anthony Fu <github@antfu.me>

--- packages/shared/utils/ast.ts
+++ packages/shared/utils/ast.ts
@@ -3,7 +3,7 @@ export * from './ast/general'
 export {
   AST_NODE_TYPES,
   AST_TOKEN_TYPES,
-} from '@typescript-eslint/utils'
+} from '@typescript-eslint/types'
 
 // eslint-disable-next-line ts/no-restricted-imports
 export * from '@typescript-eslint/utils/ast-utils'
