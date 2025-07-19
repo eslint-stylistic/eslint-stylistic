89256f6dbd6e78b8ebda0ccc1d5eba376f2fc73a
Vida Xie <vida_2020@163.com>
2025-07-19 09:02:56 +0800

    test: port cases from `eslint-plugin-antfu`

---
+++ packages/eslint-plugin/rules/list-style/list-style._jsx_.test.ts
@@ -0,0 +1,134 @@
+import { $, run } from '#test'
+import rule from './list-style'
+
+run({
+  name: 'list-newline',
+  rule,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  valid: [
+    // https://github.com/antfu/eslint-plugin-antfu/issues/14
+    {
+      code: $`
+        const a = (
+          <div>
+            {text.map((item, index) => (
+              <p>
+              </p>
+            ))}
+          </div>
+        )
+      `,
+    },
+    // https://github.com/antfu/eslint-plugin-antfu/issues/16
+    {
+      code: $`
+        function TodoList() {
+          const { data, isLoading } = useSwrInfinite(
+            (page) => ['/api/todo/list', { page }],
+            ([, params]) => getToDoList(params),
+          )
+          return <div></div>
+        }
+      `,
+    },
+    {
+      code: $`
+        function Foo() {
+          return (
+            <div 
+              className="text-white" onClick="bar"
+              style={{
+                color: 'red' 
+              }}
+            >
+              hi
+            </div>
+          );
+        }
+      `,
+    },
+  ],
+  invalid: [
+    // https://github.com/antfu/eslint-plugin-antfu/issues/14
+    {
+      code: $`
+        const a = (
+          <div>
+            {text.map((
+              item, index) => (
+              <p>
+              </p>
+            ))}
+          </div>
+        )
+      `,
+      output: $`
+        const a = (
+          <div>
+            {text.map((
+              item, 
+        index
+        ) => (
+              <p>
+              </p>
+            ))}
+          </div>
+        )
+      `,
+    },
+    {
+      code: $`
+        function Foo() {
+          return (
+            <div className="text-white"
+              onClick="bar"
+              style={{ color: 'red' }}
+            >
+              hi
+            </div>
+          );
+        }
+      `,
+      output: $`
+        function Foo() {
+          return (
+            <div className="text-white"      onClick="bar"      style={{ color: 'red' }}    >
+              hi
+            </div>
+          );
+        }
+      `,
+    },
+    {
+      code: $`
+        function Foo() {
+          return (
+            <div 
+              className="text-white" onClick="bar"
+              style={{ color: 'red' }}
+            >
+              hi
+            </div>
+          );
+        }
+      `,
+      output: $`
+        function Foo() {
+          return (
+            <div 
+              className="text-white" 
+        onClick="bar"
+              style={{ color: 'red' }}
+            >
+              hi
+            </div>
+          );
+        }
+      `,
+    },
+  ],
+})
