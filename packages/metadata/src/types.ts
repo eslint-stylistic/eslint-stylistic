f9462e11ac5b3368283ff59d860e84f8d55d1f22
Vida Xie <vida_2020@163.com>
2025-07-19 16:53:17 +0800

    feat: introduce experimental mechanism
    # Conflicts:
    #	packages/metadata/src/metadata.ts

--- packages/metadata/src/types.ts
+++ packages/metadata/src/types.ts
@@ -20,6 +20,7 @@ export interface RuleMeta {
   fixable?: 'code' | 'whitespace' | null
   docs?: {
     description?: string
-    recommended?: boolean | 'stylistic'
+    experimental?: boolean
+    recommended?: boolean
   }
 }
