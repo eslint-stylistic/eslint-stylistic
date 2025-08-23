7ddc8cd424d5da517863ad73b0bcb6506fd54afa
Vida Xie <vida_2020@163.com>
2025-08-24 00:37:57 +0800

    chore: update comment

--- packages/eslint-plugin/rules/no-extra-parens/no-extra-parens.ts
+++ packages/eslint-plugin/rules/no-extra-parens/no-extra-parens.ts
@@ -26,6 +26,18 @@ import {
 } from '#utils/ast'
 import { createRule } from '#utils/create-rule'
 
+/**
+ * extends from https://github.com/typescript-eslint/typescript-eslint/blob/bd9e490eb76705ed4dec2f3f705eb18e4fe313e0/packages/utils/src/ast-utils/predicates.ts#L57
+ *
+ * Checks if a node is a type assertion:
+ *
+ * ``` ts
+ * x as foo
+ * <foo>x
+ * x satisfies foo
+ * x!
+ * ```
+ */
 const isTypeAssertion = isNodeOfTypes([
   AST_NODE_TYPES.TSAsExpression,
   AST_NODE_TYPES.TSNonNullExpression,
