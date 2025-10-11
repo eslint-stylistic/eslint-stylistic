a9ec0de85d61c9e7452dac1942fff46684c379ed
Vida Xie <vida_2020@163.com>
2025-10-12 00:14:41 +0800

    feat: new rule list-style (#895)

--- shared/test-utils/runner.ts
+++ shared/test-utils/runner.ts
@@ -1,21 +1,27 @@
 import type { RuleTesterInitOptions, TestCasesOptions } from 'eslint-vitest-rule-tester'
-
 import tsParser from '@typescript-eslint/parser'
 import { run as _run } from 'eslint-vitest-rule-tester'
+import jsonParser from 'jsonc-eslint-parser'
 
 export * from 'eslint-vitest-rule-tester'
 
 export { unindent as $ } from 'eslint-vitest-rule-tester'
 
 export interface ExtendedRuleTesterOptions<RuleOptions = any, MessageIds extends string = string> extends RuleTesterInitOptions, TestCasesOptions<RuleOptions, MessageIds> {
-  lang?: 'js' | 'ts'
+  lang?: 'js' | 'ts' | 'json'
 }
 
 export function run<RuleOptions = any, MessageIds extends string = string>(options: ExtendedRuleTesterOptions<RuleOptions, MessageIds>) {
   return _run<RuleOptions, MessageIds>({
     recursive: false,
     verifyAfterFix: false,
-    ...(options.lang === 'js' ? {} : { parser: tsParser as any }),
+    ...(
+      options.lang === 'json'
+        ? { parser: jsonParser }
+        : options.lang === 'js'
+          ? {}
+          : { parser: tsParser }
+    ),
     ...options,
   })
 }
