cd345e222436948607e2ca85aacd1a1e8b925f91
Vida Xie <vida_2020@163.com>
2025-10-17 12:46:10 +0800

    fix(comma-dangle): support `TSFunctionType`

--- packages/eslint-plugin/rules/comma-dangle/comma-dangle._ts_.test.ts
+++ packages/eslint-plugin/rules/comma-dangle/comma-dangle._ts_.test.ts
@@ -278,5 +278,13 @@ run<RuleOptions, MessageIds>({
         },
       },
     },
+
+    {
+      code: 'type Foo = (A: number, B: number,) => void',
+      output: 'type Foo = (A: number, B: number) => void',
+      errors: [
+        { messageId: 'unexpected', line: 1, column: 17 },
+      ],
+    },
   ],
 })
