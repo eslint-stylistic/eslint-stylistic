d79521942c8f697af11f25e317df4aeb43240a99
Vida Xie <vida_2020@163.com>
2025-10-18 16:01:41 +0800

    chore: update

--- packages/eslint-plugin/rules/type-generic-spacing/type-generic-spacing.test.ts
+++ packages/eslint-plugin/rules/type-generic-spacing/type-generic-spacing.test.ts
@@ -60,7 +60,7 @@ run<RuleOptions, MessageIds>({
     ['type Foo<T=(true)> = T', 'type Foo<T = (true)> = T'],
     ['type Foo<T extends (true)=(true)> = T', 'type Foo<T extends (true) = (true)> = T'],
     ['type Foo< T, K   > = T', 'type Foo<T, K> = T', 2],
-    ['type Foo<T=false,K=1|2> = T', 'type Foo<T = false, K = 1|2> = T', 3],
+    ['type Foo<T=false, K=1|2> = T', 'type Foo<T = false, K = 1|2> = T', 2],
     ['function foo <T>() {}', 'function foo<T>() {}'],
     ['function foo< T >() {}', 'function foo<T>() {}', 2],
     [
