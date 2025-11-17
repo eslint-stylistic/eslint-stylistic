7cbc8d9f6a3f60cd58bee2ca9313ff3ac07de5c6
yosuke ota <otameshiyo23@gmail.com>
2025-11-17 23:18:23 +0900

    feat(linebreak-style): improve to make language-agnostic

--- packages/eslint-plugin/rules/linebreak-style/linebreak-style.ts
+++ packages/eslint-plugin/rules/linebreak-style/linebreak-style.ts
@@ -3,9 +3,29 @@
  * @author Erik Mueller
  */
 
+import type * as core from '@eslint/core'
+import type * as pluginKit from '@eslint/plugin-kit'
 import type { MessageIds, RuleOptions } from './types'
-import { createGlobalLinebreakMatcher } from '#utils/ast'
 import { createRule } from '#utils/create-rule'
+
+type TextSourceCode
+  = Pick<pluginKit.TextSourceCodeBase, 'getLocFromIndex' | 'lines'>
+    & core.TextSourceCode
+
+function isTextSourceCode(sourceCode: core.SourceCode & Partial<TextSourceCode>): sourceCode is TextSourceCode {
+  if (typeof sourceCode.text !== 'string') {
+    // BinarySourceCode is ignored
+    // ESLint can specify binary source code using type annotations,
+    // but it seems that there is no plugin yet that can actually use binary source code.
+    return false
+  }
+  return (
+    // If it is not a plugin kit's TextSourceCodeBase, the rule will be ignored.
+    // For the rule to work, SourceCode must implement at least `lines` and `getLocFromIndex()`.
+    typeof sourceCode.getLocFromIndex === 'function'
+    && Array.isArray(sourceCode.lines)
+  )
+}
 
 export default createRule<RuleOptions, MessageIds>({
   name: 'linebreak-style',
@@ -31,44 +51,54 @@ export default createRule<RuleOptions, MessageIds>({
   },
 
   create(context) {
-    const sourceCode = context.sourceCode
+    // Language-agnostic SourceCode access
+    const sourceCode = context.sourceCode as unknown as core.SourceCode
 
-    return {
-      Program: function checkForLinebreakStyle(node) {
-        const linebreakStyle = context.options[0] || 'unix'
-        const expectedLF = linebreakStyle === 'unix'
-        const expectedLFChars = expectedLF ? '\n' : '\r\n'
-        const source = sourceCode.getText()
-        const pattern = createGlobalLinebreakMatcher()
-        let match
-
-        let i = 0
-
-        while ((match = pattern.exec(source)) !== null) {
-          i++
-          if (match[0] === expectedLFChars)
-            continue
-
-          const index = match.index
-          const range = [index, index + match[0].length] as const
-
-          context.report({
-            node,
-            loc: {
-              start: {
-                line: i,
-                column: sourceCode.lines[i - 1].length,
-              },
-              end: {
-                line: i + 1,
-                column: 0,
-              },
-            },
-            messageId: expectedLF ? 'expectedLF' : 'expectedCRLF',
-            fix: fixer => fixer.replaceTextRange(range, expectedLFChars),
-          })
-        }
-      },
+    if (!isTextSourceCode(sourceCode)) {
+      return {}
     }
+
+    const source = sourceCode.text
+    const lines = sourceCode.lines
+
+    const linebreakStyle = context.options[0] || 'unix'
+    const expectedLF = linebreakStyle === 'unix'
+    const expectedLFChars = expectedLF ? '\n' : '\r\n'
+
+    let currentIndex = 0
+    for (let lineIndex = 0; lineIndex < lines.length - 1; lineIndex++) {
+      const line = lines[lineIndex]
+      const startIndex = currentIndex + line.length
+      const startLoc = sourceCode.getLocFromIndex(startIndex)
+
+      // Find linebreak end index
+      // startIndex is at the beginning of linebreak chars
+      let endIndex = startIndex + 1
+      let endLoc = sourceCode.getLocFromIndex(endIndex)
+      while (
+        // Still in linebreak chars
+        endLoc.line === startLoc.line
+        && endIndex < source.length
+      ) {
+        endIndex++
+        endLoc = sourceCode.getLocFromIndex(endIndex)
+      }
+
+      const linebreakChars = source.slice(startIndex, endIndex)
+      if (linebreakChars !== expectedLFChars) {
+        context.report({
+          loc: {
+            start: startLoc,
+            end: endLoc,
+          },
+          messageId: expectedLF ? 'expectedLF' : 'expectedCRLF',
+          fix: fixer => fixer.replaceTextRange([startIndex, endIndex], expectedLFChars),
+        })
+      }
+
+      currentIndex = endIndex
+    }
+
+    return {}
   },
 })
