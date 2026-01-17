bc271f728e22999c8bb8b6990290714651490ac2
Vida Xie <vida_2020@163.com>
2026-01-18 01:57:07 +0800

    refactor(line-comment-position): simplify options initialization

--- packages/eslint-plugin/rules/line-comment-position/line-comment-position.ts
+++ packages/eslint-plugin/rules/line-comment-position/line-comment-position.ts
@@ -1,7 +1,3 @@
-/**
- * @fileoverview Rule to enforce the position of line comments
- * @author Alberto Rodríguez
- */
 import type { MessageIds, RuleOptions } from './types'
 import { COMMENTS_IGNORE_PATTERN, isTokenOnSameLine } from '#utils/ast'
 import { createRule } from '#utils/create-rule'
@@ -11,11 +7,9 @@ export default createRule<RuleOptions, MessageIds>({
   name: 'line-comment-position',
   meta: {
     type: 'layout',
-
     docs: {
       description: 'Enforce position of line comments',
     },
-
     schema: [
       {
         oneOf: [
@@ -49,35 +43,27 @@ export default createRule<RuleOptions, MessageIds>({
       above: 'Expected comment to be above code.',
       beside: 'Expected comment to be beside code.',
     },
+    defaultOptions: ['above'],
   },
+  create(context, [options]) {
+    const sourceCode = context.sourceCode
 
-  create(context) {
-    const options = context.options[0]
-
-    let above
-    let ignorePattern: string
-    let applyDefaultIgnorePatterns = true
-    let customIgnoreRegExp: RegExp
-
-    if (!options || typeof options === 'string') {
-      above = !options || options === 'above'
-    }
-    else {
-      above = !options.position || options.position === 'above'
-      ignorePattern = options.ignorePattern!
-      customIgnoreRegExp = new RegExp(ignorePattern, 'u')
-
-      if (Object.hasOwn(options, 'applyDefaultIgnorePatterns'))
-        applyDefaultIgnorePatterns = options.applyDefaultIgnorePatterns!
-      else
-        applyDefaultIgnorePatterns = options.applyDefaultPatterns !== false
-
+    if (typeof options !== 'string') {
       warnDeprecatedOptions(options, 'applyDefaultPatterns', 'applyDefaultIgnorePatterns', 'line-comment-position')
     }
 
+    const {
+      position = 'above',
+      ignorePattern,
+      applyDefaultPatterns = true,
+      applyDefaultIgnorePatterns = applyDefaultPatterns,
+    } = typeof options === 'string' ? { position: options } : options!
+
+    const above = position === 'above'
+    const customIgnoreRegExp = ignorePattern ? new RegExp(ignorePattern, 'u') : null
+
     const defaultIgnoreRegExp = COMMENTS_IGNORE_PATTERN
     const fallThroughRegExp = /^\s*falls?\s?through/u
-    const sourceCode = context.sourceCode
 
     // --------------------------------------------------------------------------
     // Public
@@ -94,7 +80,7 @@ export default createRule<RuleOptions, MessageIds>({
           if (applyDefaultIgnorePatterns && (defaultIgnoreRegExp.test(node.value) || fallThroughRegExp.test(node.value)))
             return
 
-          if (ignorePattern && customIgnoreRegExp.test(node.value))
+          if (customIgnoreRegExp?.test(node.value))
             return
 
           const previous = sourceCode.getTokenBefore(node, { includeComments: true })
