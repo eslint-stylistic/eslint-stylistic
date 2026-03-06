b3a7675beb90096141eba5b0225abe3423a69e39
Vida Xie <vida_2020@163.com>
2026-03-06 21:36:12 +0800

    update

--- shared/utils/ast/general.ts
+++ shared/utils/ast/general.ts
@@ -2,7 +2,6 @@ import type { ASTNode, SourceCode, Token, Tree } from '#types'
 import type { AST_NODE_TYPES } from '@typescript-eslint/utils'
 import { isClosingParenToken, isColonToken, isCommentToken, isFunction, isOpeningParenToken, isTokenOnSameLine, LINEBREAK_MATCHER } from '@typescript-eslint/utils/ast-utils'
 import { KEYS as eslintVisitorKeys } from 'eslint-visitor-keys'
-// @ts-expect-error missing types
 import { latestEcmaVersion, tokenize } from 'espree'
 
 export const COMMENTS_IGNORE_PATTERN = /^\s*(?:eslint|jshint\s+|jslint\s+|istanbul\s+|globals?\s+|exported\s+|jscs)/u
@@ -314,7 +313,7 @@ export function isKeywordToken(token: Token | null | undefined): token is Tree.K
  * #!/usr/bin/env node
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#hashbang_comments
  */
-export function isHashbangComment(comment: Tree.Comment): Tree.Comment {
+export function isHashbangComment(comment: Tree.Comment): comment is Tree.Comment & { type: 'SheBang' | 'Hashbang' } {
   // @ts-expect-error 'Shebang' is not in the type definition
   // If a hashbang comment was passed as a token object from SourceCode,
   // its type will be "Shebang" because of the way ESLint itself handles hashbangs.
@@ -683,13 +682,13 @@ export function canTokensBeAdjacent(leftValue: Token | string, rightValue: Token
       return false
     }
 
-    const comments = tokens.comments
+    const comments = tokens.comments!
 
     leftToken = tokens[tokens.length - 1]
     if (comments.length) {
       const lastComment = comments[comments.length - 1]
 
-      if (!leftToken || lastComment.range[0] > leftToken.range[0])
+      if (!leftToken || lastComment.range![0] > leftToken.range![0])
         leftToken = lastComment
     }
   }
@@ -697,7 +696,7 @@ export function canTokensBeAdjacent(leftValue: Token | string, rightValue: Token
     leftToken = leftValue
   }
 
-  if (isHashbangComment(leftToken))
+  if (isHashbangComment(leftToken as Tree.Comment))
     return false
 
   let rightToken
@@ -712,13 +711,13 @@ export function canTokensBeAdjacent(leftValue: Token | string, rightValue: Token
       return false
     }
 
-    const comments = tokens.comments
+    const comments = tokens.comments!
 
     rightToken = tokens[0]
     if (comments.length) {
       const firstComment = comments[0]
 
-      if (!rightToken || firstComment.range[0] < rightToken.range[0])
+      if (!rightToken || firstComment.range![0] < rightToken.range![0])
         rightToken = firstComment
     }
   }
