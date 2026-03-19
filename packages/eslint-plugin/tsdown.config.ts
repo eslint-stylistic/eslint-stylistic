4ab7473745437ef99ccecd56b173b7e2314c1213
Vida Xie <vida_2020@163.com>
2026-03-20 02:36:04 +0800

    build: migrate to per-package tsdown config

--- packages/eslint-plugin/tsdown.config.ts
+++ packages/eslint-plugin/tsdown.config.ts
@@ -1,4 +1,89 @@
-import { fileURLToPath } from 'node:url'
-import { createConfig } from '../../tsdown.config.base.ts'
+import { basename, dirname } from 'node:path'
+import { env } from 'node:process'
+import { codecovRollupPlugin } from '@codecov/rollup-plugin'
+import fg from 'fast-glob'
+import { defineConfig } from 'tsdown'
+import pkg from './package.json' with { type: 'json' }
 
-export default createConfig(fileURLToPath(new URL('.', import.meta.url)))
+const rulesEntry = fg.sync(
+  'rules/**/*.ts',
+  {
+    onlyFiles: true,
+    ignore: [
+      'rules/index.ts',
+      '**/*._*_.ts',
+      '**/*.d.ts',
+      '**/*.test.ts',
+    ],
+  },
+)
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    dts: false,
+    hash: false,
+    minify: 'dce-only',
+    fixedExtension: false,
+    deps: {
+      onlyBundle: [
+        // https://github.com/eslint-stylistic/eslint-stylistic/pull/838
+        '@typescript-eslint/utils',
+        'escape-string-regexp',
+      ],
+    },
+    outputOptions: {
+      codeSplitting: {
+        groups: [
+          {
+            name: 'vendor',
+            test: /node_modules/,
+          },
+          {
+            name: 'utils',
+            test: /utils/,
+          },
+          ...rulesEntry.map(file => ({
+            name: `rules/${basename(dirname(file))}`,
+            test: new RegExp(`rules[\\/]${basename(dirname(file))}[\\/]`),
+          })),
+          {
+            name: 'configs',
+            test: /configs/,
+          },
+        ],
+      },
+    },
+    plugins: [
+      codecovRollupPlugin({
+        enableBundleAnalysis: env.CODECOV_TOKEN !== undefined,
+        bundleName: pkg.name,
+        uploadToken: env.CODECOV_TOKEN,
+        gitService: 'github',
+      }),
+    ],
+  },
+  {
+    entry: [
+      './dts/index.d.ts',
+      './dts/define-config-support.d.ts',
+      './dts/rule-options.d.ts',
+    ],
+    tsconfig: 'tsconfig.dts.json',
+    dts: {
+      dtsInput: true,
+    },
+    hash: false,
+    outDir: 'dist/dts',
+    plugins: [
+      {
+        name: 'strip-comments',
+        renderChunk(code) {
+          return code
+            .replace(/\/\* .*? \*\//g, '')
+            .replace(/\n{3,}/g, '\n\n')
+        },
+      },
+    ],
+  },
+])
